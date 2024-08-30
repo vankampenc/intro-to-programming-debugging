8ea4960ce0b7c16c0951917c9e4cd0cdfe42ef52
Christopher Van Kampen <christopheryungvankampen@gmail.com>
2024-08-30 15:27:47 -0400

    debugging fixes

--- index.js
+++ index.js
@@ -43,7 +43,8 @@ function checkGuess() {
     if (guess < targetNumber) {
       tooLowMessage.style.display = '';
     } else {
-      tooLowMessage.style.display = '';
+      // Bug - should display tooHighMessage instead of tooLowMessage
+      tooHighMessage.style.display = '';
     }
 
     const remainingAttempts = maxNumberOfAttempts - attempts;
@@ -51,8 +52,8 @@ function checkGuess() {
     numberOfGuessesMessage.style.display = '';
     numberOfGuessesMessage.innerHTML = `You guessed ${guess}. <br> ${remainingAttempts} guesses remaining`;
   }
-
-  if (attempts ==== maxNumberOfAttempts) {
+//Bug - the if condition had 4 = signs instead of 3
+  if (attempts === maxNumberOfAttempts) {
     submitButton.disabled = true;
     guessInput.disabled = true;
   }
@@ -63,25 +64,29 @@ function checkGuess() {
 }
 
 function hideAllMessages() {
-  for (let elementIndex = 0; elementIndex <= messages.length; elementIndex++) {
+  //Bug - elementIndex should have been less than messages.length instead of less than or equal to
+  for (let elementIndex = 0; elementIndex < messages.length; elementIndex++) {
     messages[elementIndex].style.display = 'none';
   }
 }
-
-funtion setup() {
+//Bug - function was spelled incorrectly
+function setup() {
   // Get random number
   targetNumber = getRandomNumber(1, 100);
   console.log(`target number: ${targetNumber}`);
 
   // Reset number of attempts
-  maxNumberOfAttempts = 0;
+  //Bug - should reset attempts not maxNumberAttempts
+  attempts = 0;
 
   // Enable the input and submit button
-  submitButton.disabeld = false;
+  submitButton.disabeld = false; // Bug this doesn't reset the button
   guessInput.disabled = false;
 
   hideAllMessages();
   resetButton.style.display = 'none';
+
+  submitButton.removeAttribute("disabled"); //Bug fix - This resets the button
 }
 
 submitButton.addEventListener('click', checkGuess);
